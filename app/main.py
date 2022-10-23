a5b49fee1927033457731dc110a40cfbadbbb0ca
tadenot <theophile.adenot@polytechnique.edu>
2022-10-23 23:45:32 +0200

    corrections in endpoints def

--- app/main.py
+++ app/main.py
@@ -1,22 +1,22 @@
 from typing import Tuple, List
 from fastapi import FastAPI
 
-from app.dependencies.function import GadjosTeam, Location
+from app.dependencies.function import GadjosTeam, LatLng, Location
 import app.dependencies.google_api as gg
 
 app = FastAPI()
 
 
 @app.post("/best-meet-up1/")
-async def best_meet_up(team: GadjosTeam) -> Tuple[Location, float]:
+async def best_meet_up1(team: GadjosTeam) -> Tuple[LatLng, float]:
     return team.find_best_meet_up1()
 
 @app.post("/best-meet-up2/")
-async def best_meet_up(team: GadjosTeam) -> Tuple[Location, float]:
+async def best_meet_up2(team: GadjosTeam) -> Tuple[LatLng, float]:
     return team.find_best_meet_up2()
 
 @app.post("/best-meet-up3/")
-async def best_meet_up(team: GadjosTeam) -> Tuple[Location, float]:
+async def best_meet_up3(team: GadjosTeam) -> Tuple[LatLng, float]:
     return team.find_best_meet_up3()
 
 @app.post("/location-autocomplete/")
